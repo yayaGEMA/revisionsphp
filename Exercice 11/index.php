fcaa591bd1fa41c157002286a2e1966e613a4b1b
Marceau.GERARD <marceaugerard1610@gmail.com>
2020-06-26 12:47:06 +0200

    Fini Exo11

--- Exercice 11/index.php
+++ Exercice 11/index.php
@@ -10,9 +10,22 @@ Exercice : Créer une fonction getGoogleLogo() qui téléchargera le logo à l'a
 // Fonction à créer ici
 //-------------------------------------------------------------------------
 
+function getGoogleLogo(){
+    // Initialize a file URL to the variable
+    $url = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
 
+    // Use basename() function to return the base name of file
+    $filename = basename($url);
 
+    if(!file_exists('logos')) {
+        mkdir('logos', 0777, true);
+    }
 
+    // Use file_get_contents() function to get the file
+    // from url and use file_put_contents() function to
+    // save the file by using base name
+    file_put_contents( 'logos/'.$filename,file_get_contents($url));
+}
 
 //-------------------------------------------------------------------------
 
