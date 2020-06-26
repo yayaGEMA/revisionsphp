fcde22c2b95a53aea803ab008821e0bb7f49aada
Marceau.GERARD <marceaugerard1610@gmail.com>
2020-06-26 11:56:11 +0200

    Fini Ex8

--- Exercice 8/index.php
+++ Exercice 8/index.php
@@ -10,9 +10,9 @@ Exercice : Créer une fonction biggestOf() qui prend 1 argument de type "array" 
 // Fonction à créer ici
 //-------------------------------------------------------------------------
 
-
-
-
+function biggestOf(array $numbers){
+    return max($numbers);
+}
 
 //-------------------------------------------------------------------------
 
